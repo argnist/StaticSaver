a77a8478cbed5a52644314d010ba1102328a0a3c
ahaller <ahaller@fairwinds.org>
2014-08-05 17:39:20 -0400

    Implemented persistent category name
    Implemented persistent category name handling by getting the category name associated with the category id. The response has also changed from a binary (1 or 0) to json.

--- core/components/staticsaver/processors/check.php
+++ core/components/staticsaver/processors/check.php
@@ -13,5 +13,16 @@ switch ($_POST['type']) {
     default: echo 1; return; break;
 }
 
+$response = array();
+
 $object = $modx->getObject($class, array('id:<' => $_POST['id'], 'static_file' => $_POST['static_file'], 'source' => $_POST['source']));
-echo !empty($object) ? 1 : 0;
+$response["result"] = (!empty($object) ? 1 : 0);
+
+if (!empty($_POST['category'])) {
+	$modCat = $modx->getObject('modCategory', $_POST['category']);
+	$category = $modCat->category;
+	$response["category"] = (!empty($category) ? $category : "");
+}
+
+header('Content-Type: application/json');
+echo json_encode($response);
